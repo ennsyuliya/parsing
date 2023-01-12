58f597c41eedee62db5c476a27e8058f09d2bfd7
enns <yuliya_enns@mail.ru>
2023-01-12 14:54:28 +0800

    jobparser

---
+++ jobparser/pipelines.py
@@ -0,0 +1,30 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+from itemadapter import ItemAdapter
+from pymongo import MongoClient
+
+
+class JobparserPipeline:
+    def __init__(self):
+        client = MongoClient('localhost', 27017)
+        self.mongo_base = client.vacancies1201
+
+
+    def process_item(self, item, spider):
+        # item['salary'] = self.process_salary(item['salary'])
+
+        collection = self.mongo_base[spider.name]
+        collection.insert_one(item)
+        return item
+
+    def process_salary(self, salary):
+        min = 0
+        max = 0
+        cur = ''
+        return min, max, cur
